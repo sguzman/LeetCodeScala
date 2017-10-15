985fd7ed67724357f9604367d89f0c73ae415930
Salvador Guzman <guzmansalv@gmail.com>
2017-10-15 09:29:40 -0700

    added another unit test

--- src/test/scala/org/github/sguzman/leetcode/twosum1/SolutionTest.scala
+++ src/test/scala/org/github/sguzman/leetcode/twosum1/SolutionTest.scala
@@ -7,8 +7,6 @@ object SolutionTest {
     val actual = Solution.twoSum(nums, target)
     Assert.assertArrayEquals(actual, expected)
   }
-
-
 }
 
 class SolutionTest {
@@ -17,6 +15,15 @@ class SolutionTest {
     val nums = Array(2, 7, 11, 15)
     val target = 9
     val expect = Array(0, 1)
+
+    SolutionTest.twoSumGeneralAssertTest(nums, target, expect)
+  }
+
+  @org.junit.Test
+  def twoSumTest2(): Unit = {
+    val nums = Array(3, 2, 4)
+    val target = 6
+    val expect = Array(1, 2)
 
     SolutionTest.twoSumGeneralAssertTest(nums, target, expect)
   }
