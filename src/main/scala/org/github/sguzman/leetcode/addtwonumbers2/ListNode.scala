c56edb5ba7a4e26085b0b2ea690514a4473b9082
Salvador Guzman <guzmansalv@gmail.com>
2017-10-15 14:47:19 -0700

    make class sealed and case

--- src/main/scala/org/github/sguzman/leetcode/addtwonumbers2/ListNode.scala
+++ src/main/scala/org/github/sguzman/leetcode/addtwonumbers2/ListNode.scala
@@ -1,6 +1,3 @@
 package org.github.sguzman.leetcode.addtwonumbers2
 
-class ListNode(var _x: Int = 0) {
- var next: ListNode = _
- var x: Int = _x
-}
+sealed case class ListNode(var x: Int = 0, var next: ListNode = null)
