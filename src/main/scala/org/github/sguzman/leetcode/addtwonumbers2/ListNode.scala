16d289039cd031170e3062bc8e823e342e75f718
Salvador Guzman <guzmansalv@gmail.com>
2017-10-16 05:32:28 -0700

    fixed up ListNode toString

--- src/main/scala/org/github/sguzman/leetcode/addtwonumbers2/ListNode.scala
+++ src/main/scala/org/github/sguzman/leetcode/addtwonumbers2/ListNode.scala
@@ -3,11 +3,11 @@ package org.github.sguzman.leetcode.addtwonumbers2
 sealed case class ListNode(var x: Int = 0, var next: ListNode = null) {
   override def toString: String = {
     val buff = new StringBuilder()
-    buff.append('(', x)
+    buff.append('(').append(x)
 
     var currNode = this.next
-    while (currNode != null) {
-      buff.append(" -> ", currNode.x)
+    while (currNode != null && currNode.next != null) {
+      buff.append(" -> ").append(currNode.x)
       currNode = currNode.next
     }
 
