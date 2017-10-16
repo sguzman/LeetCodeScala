2f6e817cc99e205962ba15b9cee7ca3d8d2b0b33
Salvador Guzman <guzmansalv@gmail.com>
2017-10-16 05:27:37 -0700

    overrode toString

--- src/main/scala/org/github/sguzman/leetcode/addtwonumbers2/ListNode.scala
+++ src/main/scala/org/github/sguzman/leetcode/addtwonumbers2/ListNode.scala
@@ -3,7 +3,16 @@ package org.github.sguzman.leetcode.addtwonumbers2
 sealed case class ListNode(var x: Int = 0, var next: ListNode = null) {
   override def toString: String = {
     val buff = new StringBuilder()
+    buff.append('(', x)
 
-    var currNode =
+    var currNode = this.next
+    while (currNode != null) {
+      buff.append(" -> ", currNode.x)
+      currNode = currNode.next
+    }
+
+    buff.append(')')
+
+    buff.toString
   }
 }
