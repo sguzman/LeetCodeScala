6b25b30e13c528f2b6d31c3f4784e7216e5f673f
Salvador Guzman <guzmansalv@gmail.com>
2017-10-17 07:47:45 -0700

    first solution attempt

--- src/main/scala/org/github/sguzman/leetcode/longestsubstringwithoutrepeatingcharacters3/Solution.scala
+++ src/main/scala/org/github/sguzman/leetcode/longestsubstringwithoutrepeatingcharacters3/Solution.scala
@@ -1,7 +1,25 @@
 package org.github.sguzman.leetcode.longestsubstringwithoutrepeatingcharacters3
 
+import scala.collection.mutable
+
 object Solution {
   def lengthOfLongestSubstring(s: String): Int = {
-    -1
+    var maxLength = 1
+    var idx = 1
+    var startIdx = 0
+
+    val mapIdx = mutable.HashMap[Char, Int]()
+    while (idx < s.length) {
+      val ch = s(idx)
+      // If haven't seen
+      if (mapIdx.getOrElse(ch, -1) >= startIdx) {
+      } else {
+        maxLength += 1
+      }
+
+      mapIdx.put(ch, idx)
+      idx += 1
+    }
+    maxLength
   }
 }
