8e8b40f40cd535c4d2d3b552046f6ce60308bc72
Salvador Guzman <guzmansalv@gmail.com>
2017-10-19 09:11:40 -0700

    added stub solution test for problem 4

---
+++ src/test/scala/org/github/sguzman/leetcode/_4medianoftwosortedarrays/SolutionTest.scala
@@ -0,0 +1,16 @@
+package org.github.sguzman.leetcode._4medianoftwosortedarrays
+
+import org.junit.Test
+
+object SolutionTest {
+  def medianOfTwoSortedArraysGeneralTest(inputA: Array[Int], inputB: Array[Int], expect: Double): Unit = {
+
+  }
+}
+
+class SolutionTest {
+  @Test
+  def medianOfTwoSortedArrays: Unit = {
+
+  }
+}
