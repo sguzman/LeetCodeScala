099146eb131ffde0e29b512cd0755fefb9dbdab5
Salvador Guzman <guzmansalv@gmail.com>
2017-11-03 04:47:11 -0700

    added stub solutions

---
+++ src/main/scala/org/github/sguzman/leetcode/_7reverseinteger/Solution.scala
@@ -0,0 +1,7 @@
+package org.github.sguzman.leetcode._7reverseinteger
+
+object Solution {
+  def reverse(x: Int): Int = {
+
+  }
+}
---
+++ src/test/scala/org/github/sguzman/leetcode/_7reverseinteger/SolutionTest.scala
@@ -0,0 +1,29 @@
+package org.github.sguzman.leetcode._7reverseinteger
+
+import org.junit.{Assert, Test}
+
+object SolutionTest {
+  def reverseIntegerGeneralTest(input1: Int, expect: Int): Unit = {
+    val actual = Solution.reverse(input1)
+    Assert.assertEquals(expect, actual)
+  }
+}
+
+class SolutionTest {
+  @Test
+  def reverseInteger1(): Unit = {
+    SolutionTest.reverseIntegerGeneralTest(123, 321)
+  }
+
+  @Test
+  def reverseInteger2(): Unit = {
+    SolutionTest.reverseIntegerGeneralTest(-123, -321)
+  }
+
+  @Test
+  def reverseInteger3(): Unit = {
+    SolutionTest.reverseIntegerGeneralTest(0, 0)
+  }
+
+
+}
