62d2cf0f06d500db7890e6d7a81036457dbcd6b9
Salvador Guzman <guzmansalv@gmail.com>
2017-11-03 20:44:45 -0700

    solution for number 10

--- src/main/scala/org/github/sguzman/leetcode/_10regularexpressionmatching/Solution.scala
+++ src/main/scala/org/github/sguzman/leetcode/_10regularexpressionmatching/Solution.scala
@@ -1,7 +1,13 @@
 package org.github.sguzman.leetcode._10regularexpressionmatching
 
 object Solution {
-  def isMatch(s: String, p: String): Boolean = {
+  def isMatch(text: String, pattern: String): Boolean = {
+    if (pattern.isEmpty) return text.isEmpty
 
+    val first = !text.isEmpty && (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.')
+    if (pattern.length >= 2 && pattern.charAt(1) == '*')
+      isMatch(text, pattern.tail.tail) || (first && isMatch(text.tail, pattern))
+    else
+      first && isMatch(text.substring(1), pattern.substring(1))
   }
 }
