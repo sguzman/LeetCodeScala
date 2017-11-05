19fe1c15a68e1b76db957dbdaffe19020d348e65
Salvador Guzman <guzmansalv@gmail.com>
2017-11-05 04:19:49 -0800

    finished solution for problem 14

--- src/main/scala/org/github/sguzman/leetcode/_14longestcommonprefix/Solution.scala
+++ src/main/scala/org/github/sguzman/leetcode/_14longestcommonprefix/Solution.scala
@@ -2,6 +2,27 @@ package org.github.sguzman.leetcode._14longestcommonprefix
 
 object Solution {
   def longestCommonPrefix(strs: Array[String]): String = {
-    ""
+    if (strs == null || strs.length == 0) return ""
+
+    val high = strs.map(_.length).min
+    if (high == 0) return ""
+
+    longestCommonPrefix(strs, 1, high)
+  }
+
+  def longestCommonPrefix(strs: Array[String], low: Int, high: Int): String = {
+    if (low > high)
+      return strs(0).substring(0, (low + high) / 2)
+
+    val middle = (low + high) / 2
+    if (isCommonPrefix(strs, middle))
+      longestCommonPrefix(strs, middle + 1, high)
+    else
+      longestCommonPrefix(strs, low, middle - 1)
+  }
+
+  private def isCommonPrefix(strs: Array[String], len: Int): Boolean = {
+    val str1 = strs(0).substring(0, len)
+    strs.forall(_.startsWith(str1))
   }
 }
