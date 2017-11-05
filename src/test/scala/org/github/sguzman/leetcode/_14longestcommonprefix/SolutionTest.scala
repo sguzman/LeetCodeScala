a715a0ea1d6ee604595df9c23a0a150a9b1eb3aa
Salvador Guzman <guzmansalv@gmail.com>
2017-11-05 04:19:57 -0800

    unit tests for 14

--- src/test/scala/org/github/sguzman/leetcode/_14longestcommonprefix/SolutionTest.scala
+++ src/test/scala/org/github/sguzman/leetcode/_14longestcommonprefix/SolutionTest.scala
@@ -6,6 +6,26 @@ class SolutionTest {
   @Test
   def longestPrefix1(): Unit = {
     val actual = Solution.longestCommonPrefix(Array("a"))
-    Assert.assertEquals(actual, "a")
+    Assert.assertEquals("a", actual)
   }
+
+  @Test
+  def longestPrefix2(): Unit = {
+    val actual = Solution.longestCommonPrefix(Array[String]())
+    Assert.assertEquals("", actual)
+  }
+
+  @Test
+  def longestPrefix3(): Unit = {
+    val actual = Solution.longestCommonPrefix(Array("ab", "abc", "abd"))
+    Assert.assertEquals("ab", actual)
+  }
+
+  @Test
+  def longestPrefix4(): Unit = {
+    val actual = Solution.longestCommonPrefix(Array("a", "b"))
+    Assert.assertEquals("", actual)
+  }
+
+
 }
