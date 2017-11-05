269d1d5aa3489be0f31a1211c74b766f47fa4afd
Salvador Guzman <guzmansalv@gmail.com>
2017-11-04 17:22:31 -0700

    solution that involves recursion

--- src/main/scala/org/github/sguzman/leetcode/_13romantointeger/Solution.scala
+++ src/main/scala/org/github/sguzman/leetcode/_13romantointeger/Solution.scala
@@ -1,7 +1,21 @@
 package org.github.sguzman.leetcode._13romantointeger
 
 object Solution {
-  def romanToInt(s: String): Int = {
+  def romanToInt(s: String, num: Int = 0): Int = {
+    if (s == null || s.isEmpty) return num
+    if (s.length == 1) {
+      return num + (if (s.head == 'I') 1
+      else if (s.head == 'V') 5
+      else if (s.head == 'X') 10
+      else if (s.head == 'L') 50
+      else if (s.head == 'C') 100
+      else if (s.head == 'D') 500
+      else 1000)
+    }
 
+    if (romanToInt(s.head.toString) < romanToInt(s.substring(1, 2)))
+      romanToInt(s.tail, num - romanToInt(s.head.toString))
+    else
+      romanToInt(s.tail, num + romanToInt(s.head.toString))
   }
 }
