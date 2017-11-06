88a57341bbb52e116a908999a704979b31094821
Salvador Guzman <guzmansalv@gmail.com>
2017-11-05 22:59:20 -0800

    finished solution stub for 15

--- src/main/scala/org/github/sguzman/leetcode/_153sum/Solution.scala
+++ src/main/scala/org/github/sguzman/leetcode/_153sum/Solution.scala
@@ -4,22 +4,30 @@ import scala.collection.mutable
 
 object Solution {
   def threeSum(nums: Array[Int]): List[List[Int]] = {
-    Nil
+    val hash = mutable.HashMap[Int,(Int,Int)]()
+    val lists = mutable.ListBuffer[List[Int]]()
+
+    (0 until nums.length - 2).foreach(idx => {
+      val numI = nums(idx)
+      ((idx + 1) until nums.length).foreach(jdx => {
+        val numJ = nums(jdx)
+
+      })
+    })
+
+    lists.toList
   }
 
   def twoSum(nums: Array[Int], target: Int): Array[Int] = {
     val mutHash = mutable.HashMap[Int,Int]()
 
-    var idx = 0
-    while (idx < nums.length) {
+    nums.indices.foreach(idx => {
       val num = nums(idx)
       if (mutHash.contains(num))
         return Array(mutHash(num), idx)
       else
         mutHash.put(target - num, idx)
-
-      idx += 1
-    }
+    })
 
     // Shouldn't get here
     Array()
