e23044092efce6d712c6db3349093e11939015e8
Salvador Guzman <guzmansalv@gmail.com>
2017-11-06 14:57:26 -0800

    a more correct solution

--- src/main/scala/org/github/sguzman/leetcode/_153sum/Solution.scala
+++ src/main/scala/org/github/sguzman/leetcode/_153sum/Solution.scala
@@ -4,32 +4,34 @@ import scala.collection.mutable
 
 object Solution {
   def threeSum(nums: Array[Int]): List[List[Int]] = {
-    val hash = mutable.HashMap[Int,(Int,Int)]()
-    val lists = mutable.ListBuffer[List[Int]]()
+    val set = mutable.HashMap[Int,mutable.HashSet[Int]]()
+    val lists = mutable.HashSet[Set[(Int,Int)]]()
+    nums.indices.foreach(idx => {
+      val num = nums(idx)
+      if (set.contains(num)) {
+        set(num).add(idx)
+      } else {
+        set.put(num, mutable.HashSet(idx))
+      }
+    })
 
-    (0 until nums.length - 2).foreach(idx => {
+
+    nums.indices.foreach(idx => {
       val numI = nums(idx)
       ((idx + 1) until nums.length).foreach(jdx => {
         val numJ = nums(jdx)
-
+        val numH = 0 - (numI + numJ)
+        if (set.contains(numH)) {
+          val filtered = set(numH).filter(s => s != idx && s != jdx)
+          if (filtered.nonEmpty) {
+            val hdx = filtered.head
+            lists.add(Set((numH, hdx), (numI, idx), (numJ, jdx)))
+            set(numH).remove(hdx)
+          }
+        }
       })
     })
 
-    lists.toList
-  }
-
-  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
-    val mutHash = mutable.HashMap[Int,Int]()
-
-    nums.indices.foreach(idx => {
-      val num = nums(idx)
-      if (mutHash.contains(num))
-        return Array(mutHash(num), idx)
-      else
-        mutHash.put(target - num, idx)
-    })
-
-    // Shouldn't get here
-    Array()
+    lists.toList.map(_.toList.map(_._1))
   }
 }
