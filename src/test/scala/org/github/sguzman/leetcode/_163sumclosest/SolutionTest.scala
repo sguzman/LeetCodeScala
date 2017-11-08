cb6ed3fa861026cddd2b484c9d70a4d196f54327
Salvador Guzman <guzmansalv@gmail.com>
2017-11-08 13:58:26 -0800

    added problem 16 unit test cases

---
+++ src/test/scala/org/github/sguzman/leetcode/_163sumclosest/SolutionTest.scala
@@ -0,0 +1,57 @@
+package org.github.sguzman.leetcode._163sumclosest
+
+import org.junit.{Assert, Test}
+
+class SolutionTest {
+  @Test
+  def threeSumClosest1(): Unit = {
+    val s = Array(-1, 2, 1, -4)
+    val target = 1
+    val expected = 2
+
+    val actual = Solution.threeSumClosest(s, target)
+    Assert.assertEquals(expected, actual)
+  }
+
+  @Test
+  def threeSumClosest2(): Unit = {
+    val s = Array(0, 0, 0)
+    val target = 1
+    val expected = 0
+
+    val actual = Solution.threeSumClosest(s, target)
+    Assert.assertEquals(expected, actual)
+  }
+
+  @Test
+  def threeSumClosest3(): Unit = {
+    val s = Array(0, 1, 2)
+    val target = 1
+    val expected = 3
+
+    val actual = Solution.threeSumClosest(s, target)
+    Assert.assertEquals(expected, actual)
+  }
+
+  @Test
+  def threeSumClosest4(): Unit = {
+    val s = Array(1, 1, 1, 1)
+    val target = 0
+    val expected = 3
+
+    val actual = Solution.threeSumClosest(s, target)
+    Assert.assertEquals(expected, actual)
+  }
+
+  @Test
+  def threeSumClosest5(): Unit = {
+    val s = Array(1, 1, 1, 0)
+    val target = -100
+    val expected = 2
+
+    val actual = Solution.threeSumClosest(s, target)
+    Assert.assertEquals(expected, actual)
+  }
+
+
+}
