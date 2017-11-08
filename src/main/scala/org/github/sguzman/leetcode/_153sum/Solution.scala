fa3614a43a2f650d9f007c0f00c453091916b776
Salvador Guzman <guzmansalv@gmail.com>
2017-11-08 02:00:13 -0800

    fixed up solution

--- src/main/scala/org/github/sguzman/leetcode/_153sum/Solution.scala
+++ src/main/scala/org/github/sguzman/leetcode/_153sum/Solution.scala
@@ -4,36 +4,33 @@ import scala.collection.mutable
 
 object Solution {
   def threeSum(nums: Array[Int]): List[List[Int]] = {
-    val set = mutable.HashMap[Int,mutable.HashSet[Int]]()
-    val lists = mutable.HashSet[Set[(Int,Int)]]()
-    val values = mutable.HashSet[Set[Int]]()
-    nums.indices.foreach(idx => {
-      val num = nums(idx)
-      if (set.contains(num)) {
-        set(num).add(idx)
-      } else {
-        set.put(num, mutable.HashSet(idx))
+    val lists = mutable.ListBuffer[List[Int]]()
+    val sort = nums.sorted
+
+    (0 until (sort.length - 2)).foreach(idx => {
+      if (idx == 0 || (idx > 0 && sort(idx) != sort(idx - 1))) {
+        var jdx = idx + 1
+        var kdx = sort.length - 1
+        val partial = 0 - sort(idx)
+
+        while (jdx < kdx) {
+          if (sort(jdx) + sort(kdx) == partial) {
+            lists.append(List(sort(idx), sort(jdx), sort(kdx)))
+            while (jdx < kdx && sort(jdx) == sort(jdx + 1)) jdx += 1
+            while (jdx < kdx && sort(kdx) == sort(kdx - 1)) jdx += 1
+
+            jdx += 1
+            kdx -= 1
+          }
+          else if (sort(jdx) + sort(kdx) > partial) {
+            kdx -= 1
+          } else {
+            jdx += 1
+          }
+        }
       }
     })
 
-
-    nums.indices.foreach(idx => {
-      val numI = nums(idx)
-      ((idx + 1) until nums.length).foreach(jdx => {
-        val numJ = nums(jdx)
-        val numH = 0 - (numI + numJ)
-        if (set.contains(numH)) {
-          val hdx = set(numH).head
-          if (Set(hdx, idx, jdx).size == 3) {
-            if (!values.contains(Set(numH, numJ, numI))) {
-              lists.add(Set((numH, hdx), (numI, idx), (numJ, jdx)))
-              values.add(Set(numH, numJ, numI))
-            }
-          }
-        }
-      })
-    })
-
-    lists.toList.map(_.toList.map(_._1))
+    lists.toList
   }
 }
