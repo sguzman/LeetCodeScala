061e9bc36cf7043ab3d4ebcb5f17f8ce7a5b7139
Salvador Guzman <guzmansalv@gmail.com>
2017-11-08 14:43:02 -0800

    got a working solution for 16

--- src/main/scala/org/github/sguzman/leetcode/_163sumclosest/Solution.scala
+++ src/main/scala/org/github/sguzman/leetcode/_163sumclosest/Solution.scala
@@ -7,7 +7,34 @@ object Solution {
     } else if (nums.forall(_ == nums.head)) {
       nums.head * 3
     } else {
-      -1
+      var min = math.max(math.abs(target - Int.MaxValue), math.abs(target - Int.MinValue))
+      var summation = 0
+      val sort = nums.sorted
+      for (idx <- 0 until sort.length - 2) {
+        var jdx = idx + 1
+        var kdx = sort.length - 1
+        while (jdx < kdx) {
+          val sum = sort(idx) + sort(jdx) + sort(kdx)
+          val diff = math.abs(target - sum)
+
+          if (diff == 0) {
+            return sum
+          }
+
+          if (diff < min) {
+            min = diff
+            summation = sum
+          }
+
+          if (sum > target) {
+            kdx -= 1
+          } else {
+            jdx += 1
+          }
+        }
+      }
+
+      summation
     }
   }
 }
