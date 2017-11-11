0ec274ee26a94f5e2a6898e898dc271d25bb7a4f
Salvador Guzman <guzmansalv@gmail.com>
2017-11-11 11:37:45 -0800

    fixed test cases to be in order

--- src/test/scala/org/github/sguzman/leetcode/_174sum/SolutionTest.scala
+++ src/test/scala/org/github/sguzman/leetcode/_174sum/SolutionTest.scala
@@ -6,7 +6,11 @@ object SolutionTest {
   def fourSumGeneralTest(s: Array[Int], t: Int, expect: List[List[Int]]): Unit = {
     val actual = Solution.fourSum(s, t)
     Assert.assertEquals("Lengths of arrays are not equal", expect.length, actual.length)
-    expect.map(_.toArray).zip(actual.map(_.toArray)).foreach((tup: (Array[Int], Array[Int])) => Assert.assertArrayEquals(tup._1, tup._2))
+    expect
+      .map(_.toArray)
+      .zip(actual
+        .map(_.toArray))
+      .foreach((tup: (Array[Int], Array[Int])) => Assert.assertArrayEquals(tup._1, tup._2))
   }
 }
 
@@ -17,9 +21,9 @@ class SolutionTest {
     val t = 0
 
     val expect = List(
-      List(-1, 0, 0, 1),
       List(-2,-1, 1, 2),
-      List(-2, 0, 0, 2)
+      List(-2, 0, 0, 2),
+      List(-1, 0, 0, 1)
     )
 
     SolutionTest.fourSumGeneralTest(s, t, expect)
