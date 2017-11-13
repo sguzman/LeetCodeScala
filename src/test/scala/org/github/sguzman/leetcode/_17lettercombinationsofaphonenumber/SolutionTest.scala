a59d476b3a3bc96d0af7bc7417a988c619069c48
Salvador Guzman <guzmansalv@gmail.com>
2017-11-13 00:06:37 -0800

    convert to string in general test method

--- src/test/scala/org/github/sguzman/leetcode/_17lettercombinationsofaphonenumber/SolutionTest.scala
+++ src/test/scala/org/github/sguzman/leetcode/_17lettercombinationsofaphonenumber/SolutionTest.scala
@@ -2,13 +2,20 @@ package org.github.sguzman.leetcode._17lettercombinationsofaphonenumber
 
 import org.junit.{Assert, Test}
 
+object SolutionTest {
+  def testGeneral(input: String, expect: List[String]): Unit = {
+    val actual = Solution.letterCombinations(input)
+    Assert.assertEquals(expect.mkString("[", ", ", "]"), actual.mkString("[", ", ", "]"))
+  }
+}
+
 class SolutionTest {
   @Test
   def letterCombinationsOfAPhoneNumberTest1(): Unit = {
     val input = "23"
-    val expect = List("ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf").mkString("[", ", ", "]")
+    val expect = List("ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf")
 
-    val actual = Solution.letterCombinations(input)
-    Assert.assertEquals(expect, actual.mkString("[", ", ", "]"))
+    SolutionTest.testGeneral(input, expect)
+    )
   }
 }
